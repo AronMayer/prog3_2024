b4fe03cf008f99df81faa2e0171cffe6fea681c7
Aron Matrix <lautaron1727@gmail.com>
2024-05-15 21:12:49 -0300

    MI Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Pagina Primer commit</title>
+</head>
+<body>
+    <?php
+        print  "Mi Primera pagina"
+    ?>
+</body>
+</html>
